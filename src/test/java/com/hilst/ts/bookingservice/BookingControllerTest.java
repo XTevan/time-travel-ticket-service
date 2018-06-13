04eaf076ccca0b2ef9a31dd7b0ea02e0d3d080e2
Estevão Hilst <xtevan@gmail.com>
2018-06-13 13:27:26 -0300

    Refactored

--- src/test/java/com/hilst/ts/bookingservice/BookingControllerTest.java
+++ src/test/java/com/hilst/ts/bookingservice/BookingControllerTest.java
@@ -7,15 +7,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import javax.servlet.ServletContext;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
@@ -30,56 +25,56 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(value=BookingController.class,secure = false)
+@WebMvcTest(value = BookingController.class, secure = false)
 public class BookingControllerTest {
-	private static final String PGI = "Jones1234";
-	private static final Long ID_TICKET = 1L;
-	private static final LocalDate DATE_TICKET = LocalDate.of(1892,1,1);
-	private static final String PLACE = "Piccadilly Circus, London";
-	private static final Ticket TICKET = new Ticket(ID_TICKET,PGI,PLACE,DATE_TICKET);
-	private static final List<Ticket> TICKETS = Collections.singletonList(TICKET);
+    private static final String PGI = "Jones1234";
+    private static final Long ID_TICKET = 1L;
+    private static final LocalDate DATE_TICKET = LocalDate.of(1892, 1, 1);
+    private static final String PLACE = "Piccadilly Circus, London";
+    private static final Ticket TICKET = new Ticket(ID_TICKET, PGI, PLACE, DATE_TICKET);
+    private static final List<Ticket> TICKETS = Collections.singletonList(TICKET);
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@MockBean
-	private BookingService service;
+    @MockBean
+    private BookingService service;
 
 
-	@Test
-	public void findAllShouldCallServiceOnceAndReturn() throws Exception {
-		when(service.findAll()).thenReturn(TICKETS);
+    @Test
+    public void findAllShouldCallServiceOnceAndReturn() throws Exception {
+        when(service.findAll()).thenReturn(TICKETS);
 
-		mockMvc.perform(get("/api/3ts")
-						.contentType(APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$",hasSize(1)))
-				.andExpect(jsonPath("$[0].place",is(PLACE)));
-		verify(service,times(1)).findAll();
-		verifyNoMoreInteractions(service);
-	}
+        mockMvc.perform(get("/api/3ts")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].place", is(PLACE)));
+        verify(service, times(1)).findAll();
+        verifyNoMoreInteractions(service);
+    }
 
-	@Test
-	public void findOneShouldCallServiceOnceAndReturnModel() throws Exception {
-		when(service.findById(ID_TICKET)).thenReturn(Optional.of(TICKET));
+    @Test
+    public void findOneShouldCallServiceOnceAndReturnModel() throws Exception {
+        when(service.findById(ID_TICKET)).thenReturn(Optional.of(TICKET));
 
-		mockMvc.perform(get("/api/3ts/1")
-				.contentType(APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.place",is(PLACE)));
-		verify(service,times(1)).findById(ID_TICKET);
-		verifyNoMoreInteractions(service);
-	}
+        mockMvc.perform(get("/api/3ts/1")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.place", is(PLACE)));
+        verify(service, times(1)).findById(ID_TICKET);
+        verifyNoMoreInteractions(service);
+    }
 
-	@Test
-	public void findOneShouldCallServiceOnceAndReturnNotFound() throws Exception {
-		when(service.findById(ID_TICKET)).thenReturn(Optional.empty());
+    @Test
+    public void findOneShouldCallServiceOnceAndReturnNotFound() throws Exception {
+        when(service.findById(ID_TICKET)).thenReturn(Optional.empty());
 
-		mockMvc.perform(get("/api/3ts/1")
-				.contentType(APPLICATION_JSON))
-				.andExpect(status().isNotFound());
-		verify(service,times(1)).findById(ID_TICKET);
-		verifyNoMoreInteractions(service);
-	}
+        mockMvc.perform(get("/api/3ts/1")
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+        verify(service, times(1)).findById(ID_TICKET);
+        verifyNoMoreInteractions(service);
+    }
 
 }
